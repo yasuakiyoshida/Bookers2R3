b9d45b8a7d3b021227af66c7b43721582bdc2796
Yasuaki Yoshida <yasu5293koh@gmail.com>
2020-11-08 03:17:28 +0000

    [Add] AboutページのURLを/homes/aboutから/home/aboutに変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :books, except: [:new]
-  resources :homes, except: [:index,:create, :new, :edit, :show, :update, :destroy] do
+  resources :homes, path: 'home', except: [:index, :create, :new, :edit, :show, :update, :destroy] do
     collection do
       get 'about'
     end
