3f0ccb79ceb81ce2d6599cf969bf3d5e957a6ff1
Yasuaki <yasu5293koh@gmail.com>
2020-12-02 08:25:11 +0000

    コメント機能：エラーメッセージ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,12 +1,15 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id]) # パラメータ(book_id)で指定された投稿を検索して、変数に格納
-    comment = BookComment.new(book_comment_params) # commentカラム情報取得
-    comment.user_id = current_user.id # user_idカラム情報取得
-    comment.book_id = book.id # book_idカラム情報取得
-    comment.save # book_comment情報揃ったのでセーブ
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id]) # ビューに渡すからインスタンス変数に
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to book_path(@book)
+    else
+      render 'books/show'
+    end
   end
   
   def destroy
