9c40165824c121a7f763f9461924820b6b93b841
Yasuaki <yasu5293koh@gmail.com>
2020-12-02 05:47:28 +0000

    コメント機能：削除機能追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,17 +2,17 @@ class BookCommentsController < ApplicationController
   
   def create
     book = Book.find(params[:book_id]) # パラメータ(book_id)で指定された投稿を検索して、変数に格納
-    comment = current_user.book_comments.new(book_comment_params) # comment = BookComment.new(book_comment_params) \ comment.user_id = current.user.idと同じ -->
-    # comment = BookComment.new(book_comment_params) # コメントを生成
-    # comment.user_id = current.user.id # 生成したコメントのユーザーIDにコメント投稿者のIDを設定
-    comment.book_id = book.id # 生成したコメントの投稿(book)IDにパラメータで渡された投稿IDを設定
-    comment.save
-    redirect_to book_path(book) # 投稿詳細画面にリダイレクト
+    comment = BookComment.new(book_comment_params) # commentカラム情報取得
+    comment.user_id = current_user.id # user_idカラム情報取得
+    comment.book_id = book.id # book_idカラム情報取得
+    comment.save # book_comment情報揃ったのでセーブ
+    redirect_to book_path(book)
   end
   
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy # パラメータ(id,book_id)で指定されたコメントと投稿を検索して削除(URLにidが2つ)
-    redirect_back fallback_location: @book.id
+    @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @comment.destroy
+    redirect_to book_path(params[:book_id])
   end
   
   private
